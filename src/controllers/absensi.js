e25e277a970ead0ecde8002a83fa20861efa67df
Guntursap <gunturgagah1@gmail.com>
2025-02-17 17:54:03 +0700

    17 feb 2025

--- src/controllers/absensi.js
+++ src/controllers/absensi.js
@@ -1645,7 +1645,7 @@ module.exports = {
     const convertYear = tahun.substring(2, 4);
     const convertBulan = array[1];
   
-    const namaDatabaseDynamic = `${database}_hrm${convertYear}${convertBulan}`;
+    const namaDatabaseDynamic = `${database}_hrm2502`;
     
     var startDate=req.body.start_date;
     var endDate=req.body.end_date;
